c75a1ebb912122058db2dc2abc8a3affe45942c9
Dayvid Santana <dayvidcomerce@gmail.com>
2024-04-24 13:49:05 -0300

    feat: mainSection component added

--- src/app/components/main/index.tsx
+++ src/app/components/main/index.tsx
@@ -1,11 +1,23 @@
-import Styles from './styles.module.scss';
+import Image from 'next/image';
+import Logo from '../../favicon.ico';
+import Style from './styles.module.scss';
 
 
 const MainSection = () => {
-    const text = '';
+    const text = 'Guiados pela Fé, Inspirados pelo Amor a primeira igreja batista em Augusto Franco Cultivamos a Esperança e Construímos um Caminho de Transformação. Junte-se a Nós, onde a Fé Ganha Vida e o Amor Floresce';
 
     return (
-        <div className={Styles.container}>
+        <div className={Style.container}>
+            <Image
+                src={Logo}
+                className={Style.logoTipo}
+                alt={'Foto na frente da igreja'}
+            />
+            <Image
+                src={Logo}
+                className={Style.logoTipo}
+                alt={'Foto no projeto esperança'}
+            />
             <p>
                 {text}
             </p>
